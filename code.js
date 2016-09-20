bebfd40c19257d3c66907abdd801119f02556d6f
Vishnu Airy (Aero-vision) <andrew@e4b.us>
2016-09-20 18:53:27 +0000

    Update code.js

--- code.js
+++ code.js
@@ -1,22 +1,15 @@
 
-function largestOfFour(arr) {
-  // You can do this!
-  var maxArray = [];
-  var biggestInt;
-  function findMax(arr){
-    for(var i = 0; i < arr.length; i++){
-      console.log(maxArray);
-      for(var j = 0; j <= arr[i].length; j++){
-        if (arr[i][j] === arr[i][0 ])  biggestInt = arr[i][j];
-        if (arr[i][j] !== arr[i][0] && arr[i][j] >= biggestInt) biggestInt = arr[i][j];
-        console.log(biggestInt);
-      }
-    maxArray.push(biggestInt);                    
-    }
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+  var counter = 0;
+  var repeatStr = str;    
+  if (num < 0)repeatStr = "";
+  while (counter < num && num !==0){
+    repeatStr += str;
+    counter++;
   }
-  
-  findMax(arr);
-  return maxArray;
- }
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+  return repeatStr;
+}
+
+repeatStringNumTimes("*", 3);
